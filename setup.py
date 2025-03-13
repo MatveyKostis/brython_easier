076d371988743f3a00820a0f0bf9d3f9568a3b45
Komaru <matveykostiapk@gmail.com>
2025-03-13 19:35:40 +0200

    add description for pip

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='brython_easier',
     version='0.1.1',
     packages=[''],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/MatveyKostis/brython_easier',
     license='GPL-3.0 license',
     author='MatveyKostis',
