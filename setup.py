648ea4931a27473c02732fa9fe7a24d340743a62
Komaru <matveykostiapk@gmail.com>
2025-03-13 19:48:16 +0200

    uh, idk already

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='brython_easier',
-    version='0.1.3',
+    version='0.1.4',
     packages=[''],
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
